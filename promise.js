cff73cdc307602bd15568d519771016dd3238b2f
Chandra A <chandra.aripin@gmail.com>
2022-09-16 10:57:04 +0700

    feat: finishing challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  const array1 = await promiseTheaterIXX();
+  const array2 = await promiseTheaterVGC();
+  
+  return new Promise((resolve) => {
+    let total = 0;
+    array1.forEach(data => {
+      if(data.hasil === emosi)
+        total += 1;
+    });
+    array2.forEach(data => {
+      if(data.hasil === emosi)
+        total += 1;
+    });
+
+    return resolve(total);
+  });
+};
 
 module.exports = {
   promiseOutput,
